014e7925c9d7dd638fecde522517498b24581ecd
Jimmy Oty <oduorjames@hotmail.com>
2022-10-29 13:20:41 +0300

    Revert "initial commit "

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from urllib import response
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home(request):
-    return HttpResponse('<h1>Hello SpaceyaTech</h1>')
