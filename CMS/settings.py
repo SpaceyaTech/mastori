79dbc55e4059b8917e163384727fce886826a8bf
root <root@DESKTOP-TI71NAL.localdomain>
2022-11-04 13:03:20 -0700

    Adding django_rest framework to the project

--- CMS/settings.py
+++ CMS/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+# Adding djangorestframework to the poject
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
