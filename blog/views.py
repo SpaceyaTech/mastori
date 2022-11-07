26c18d98dcfae65dbbcd7029dbde07c42aabd367
Jimmy Tron <oduorjames@hotmail.com>
2022-11-07 21:06:05 +0300

    minor tweak to the views retun msg

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
-from urllib import response
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello SpaceyaTech</h1>')
+    return HttpResponse('<h1>SpaceyaTech Blog CMS</h1>')
