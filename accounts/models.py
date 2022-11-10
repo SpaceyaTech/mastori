3d189b02690c132bd22fd361af1f6e5aeb3e7f8f
Hellen Wainaina <hellenwain@gmail.com>
2022-11-10 22:18:31 +0300

    creating accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=30, blank=True)
+    verification_code = models.CharField(max_length=100)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
