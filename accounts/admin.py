16ce986551b623cb65f0c2031d875e926baa5eac
Hellen Wainaina <hellenwain@gmail.com>
2022-11-10 17:10:05 +0300

    creating user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'is_staff', 'is_active',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username','password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=30, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
