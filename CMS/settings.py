4e947ecc2ded41a80b33d731aea92228c9303cbe
Hellen Wainaina <hellenwain@gmail.com>
2022-11-10 16:20:08 +0300

    creating accounts app for the user model

--- CMS/settings.py
+++ CMS/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 # Adding djangorestframework to the poject
+    
     'rest_framework',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
