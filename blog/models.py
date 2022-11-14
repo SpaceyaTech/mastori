33b6d56696e6ecc39b884516ff65f4ed1afe8989
joseph thuku <jthuku490@gmail.com>
2022-11-14 15:21:20 +0300

    worked on the account and blogpost models

--- blog/models.py
+++ blog/models.py
@@ -1,28 +1,25 @@
 from django.db import models
-#from django.contrib.auth.models import User
-
-class user(models.Model):
-    user_id = models.AutoField(primary_key=True, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=60)
-    phone_number = models.CharField(max_length=45)
-    verification_code = models.CharField(unique=True)
-    date_created = models.DateField(auto_now_add=True)
-    usercol = models.CharField(max_length=45)
-
-
-    def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
-
+# note that im useing the Django inbuild user model
 class account(models.Model):
     id = models.AutoField(primary_key= True, unique=True)
     name = models.CharField(unique=True, max_length=50)
     createdAt = models.DateField(auto_now_add=True)
     updatedAt = models.DateField(auto_now=True)
-    user_id = models.ForeignKey(user, on_delete=models.CASCADE)
     display_photo = models.ImageField(upload_to="path/to/avatars/", null=True, blank=True)
     bio_data = models.CharField(max_length=1000)
-
+    """
+    note the user is from django builtin user model
+    i'm using foreignkey which allows one user to have many account
+"""
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.name
+
+class blogposts(models.Model):
+    id = models.AutoField(primary_key=True)
+    post_title = models.CharField(max_length=500)
+    blog_description = models.TextField()
+    content = models.TextField
+    blogpostscol = models.CharField(max_length=45)
+    account_name = models.ForeignKey('account.name',on_delete=models.CASCADE)
