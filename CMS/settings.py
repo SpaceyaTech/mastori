768e0d5522edab4b9679baf13d39fdcb0f44cc54
carolyne <pcmaunda@gmail.com>
2022-11-15 09:49:52 +0300

    added auth user model- setting

--- CMS/settings.py
+++ CMS/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
      'blog'
 ]
 
+AUTH_USER_MODEL = "blog.User"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
