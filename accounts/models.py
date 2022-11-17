88d0ede7045c38adc8310859fb2e0b863183fc13
Hellen Wainaina <hellenwain@gmail.com>
2022-11-17 13:16:58 +0300

    changing the account model from the blog app to account app

--- accounts/models.py
+++ accounts/models.py
@@ -11,10 +11,22 @@ REQUIRED_FIELDS - the required fields to create a superuser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=30, blank=True)
-    verification_code = models.CharField(max_length=100)
+    verification_code = models.CharField(max_length=100, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)#Referencing the customized user
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    display_picture = models.ImageField()
+    bio = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
