d7be08844b33db5ca53a17cbf01a27accd41b382
Jimmy Oty <oduorjames@hotmail.com>
2022-11-25 14:39:31 +0300

    Revert "Transfer all tests to a tests module  (#48)"
    This reverts commit 332dd7e03bc2262a4fcda08446e187713914c7f7.

--- CMS/urls.py
+++ CMS/urls.py
@@ -13,16 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-
-    #REST FRAMEWORK URLS
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/post/', include('post.api.urls','blog_api')),
 ]
-
--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Author
-# Register your models here.
 
-admin.site.register([Author])
+# Register your models here.
