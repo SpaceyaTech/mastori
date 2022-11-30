17f61b85128f3555c084296f067818fb620cd63c
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-11-30 14:28:22 +0100

    Resolve merge conflict

--- CMS/settings.py
+++ CMS/settings.py
@@ -90,6 +90,14 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+DATABASES = {
+    'default': {
         "ENGINE": 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
