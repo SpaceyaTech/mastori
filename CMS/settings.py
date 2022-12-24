fabc96ca5c26de97d60d5c0f5187969c4d9eaeb2
Kipkoech Sang <sangkipkoech@gmail.com>
2022-12-24 23:25:50 +0300

    Blogposts model (#69)
    * created blogposts models
    
    * resolving CI merge conflicts
    
    * resolving CI merge conflicts

--- CMS/settings.py
+++ CMS/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-# Adding djangorestframework to the poject
-    
+    # Adding djangorestframework to the poject
+
     'rest_framework',
     'accounts',
     'blog',
-   'phonenumber_field',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -125,10 +125,12 @@ PHONENUMBER_DEFAULT_REGION = 'KE'
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
+
+                          # Default primary key field type
+                          # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+                          )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blogpost
 
 # Register your models here.
+admin.site.register(Blogpost)
