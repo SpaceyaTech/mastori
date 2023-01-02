4fe12a4c582534e68050dd69e10896f21498ffe9
sangkips <sangkipkoech@gmail.com>
2023-01-02 12:04:52 +0300

    customized simple JWT token claims to enable token decoding

--- CMS/urls.py
+++ CMS/urls.py
@@ -15,21 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+from accounts.views import MyTokenObtainPairView
+
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
     path("", include("accounts.urls"))
 ] + static(settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT)
+           document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "SpaceYaTech CMS Admin"
 admin.site.site_title = "SpaceYaTech Admin Portal"
--- accounts/views.py
+++ accounts/views.py
@@ -3,29 +3,58 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from accounts.models import *
 from .serializers import *
 from .models import *
 
+""" Api view for customizing token claims, this is useful when it comes to logging in user in the frontend """
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["email"] = user.email,
+        token["username"] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 """Api View for listing all users and retrieving specific users using their id's"""
+
+
 class UserViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
 
     def get_queryset(self):
         if self.request.method == 'GET':
-            return User.objects.annotate(number_of_accounts=Count('account'))#Adds a new column that counts the number of accounts.
+            # Adds a new column that counts the number of accounts.
+            return User.objects.annotate(number_of_accounts=Count('account'))
         return User.objects.all()
 
 
 """Api view to be used when a user first registers to the system"""
+
+
 class RegisterAccountViewSet(CreateModelMixin, GenericViewSet):
     queryset = Account.objects.all()
     serializer_class = UserAccountRegistrationSerializer
 
 
 """Api view for a user to add another new account"""
+
+
 class AddUserAccountViewSet(ModelViewSet):
     serializer_class = AddAccountSerializer
     permission_classes = [IsAuthenticated]
