86a82e91711c0409599981361dcb6dbb12830586
JimmyTron <oduorjames@hotmail.com>
2023-01-06 09:34:59 +0300

    refactor tests

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -12,7 +12,7 @@ class UserTestCase(TestCase):
         user.set_password('password12!')
         user.save()
         
-        self.assertEqual(str(user),'Team Rio')
+        self.assertEqual(str(user.first_name+' '+user.last_name),'Team Rio')
 
 
 class AccountTestCase(TestCase):
@@ -22,5 +22,5 @@ class AccountTestCase(TestCase):
         account = Account.objects.create (user=user, name = 'space')
         account.save()
         
-        self.assertEqual(str(account),"space")
+        self.assertEqual(str(account.name),"space")
 
