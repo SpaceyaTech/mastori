b7928a1addf53ae941bb1b9037de6954dfc72e85
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-01-23 15:08:42 +0100

    testing accounts serilaizier

--- accounts/tests/test_serializers.py
+++ accounts/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from accounts.models import Account, User
-from accounts.serializers import AccountDetailSerializer, UserSerializer, UserDetailsSerializer
+from accounts.serializers import AccountDetailSerializer, UserSerializer, UserDetailsSerializer, AddAccountSerializer
+
 
 class TestAccountDetailSerializer(TestCase):
     def setUp(self):
@@ -26,20 +27,21 @@ class TestAccountDetailSerializer(TestCase):
 
 class TestUserSerializer(TestCase):
     def setUp(self):
+        # Create a user instance
         self.user = User.objects.create(
             username='TeamRio',
             email = 'teamrio@space.com',
             first_name = 'Team',
             last_name = 'Rio',
-            password = "password",
+            password = "password123",
             )
-        # first account instance
+        # create first account instance
         self.account1 = Account.objects.create(
             user = self.user,
             account_name= "default1",
             bio = "default statement1",
         )
-        #'second account instance
+        #create second account instance
         self.account2 = Account.objects.create(
             user = self.user,
             account_name= "default2",
@@ -62,15 +64,17 @@ class TestUserSerializer(TestCase):
 
 class TestUserDetailsSerializer(TestCase):
     def setUp(self):
+        # Create a user instance
         self.user = User.objects.create(
             username='TeamRio',
             email = 'teamrio@space.com',
             first_name = 'Team',
             last_name = 'Rio',
-            phone_number =  "+23434567890",
-            password = "password",
+            phone_number =  "+254729111812",
+            password = "password123",
         )
 
+        # Create an account instance for the user
         self.account = Account.objects.create(
         user = self.user,
         account_name= "default",
@@ -78,12 +82,40 @@ class TestUserDetailsSerializer(TestCase):
         )
 
     def test_user_details_serialization(self):
+        # Create an instance of the UserDetailsSerializer with the user instance
         serializer = UserDetailsSerializer(instance=self.user)
         data = serializer.data
+        # Assert that the serialized data contains the expected values
         assert data["first_name"] == 'Team'
         assert data["last_name"] == 'Rio'
         assert data["username"] == 'TeamRio'
         assert data["email"] == 'teamrio@space.com'
-        assert data["phone_number"] == "+23434567890"
-        assert "pasword" not in data
+        assert data["phone_number"] == "+254729111812"
+        assert "pasword" not in data # Assert that the password is not present in the serialized data
 
+
+class AddAccountSerializerTest(TestCase):
+    def setUp(self):
+        # create a new user
+        self.user = User.objects.create_user(
+            username = "TeamRio",
+            email='teamrio@space.com', 
+            password='testpassword'
+        )
+        self.data = {
+            'account_name': 'default',
+            'bio': 'default statement.',
+        }
+        self.context = {'user_id': self.user.id}
+
+    def test_valid_data(self):
+        # create an instance of the serializer with the test data and context
+        serializer = AddAccountSerializer(data=self.data, context=self.context)
+        # assert that the serializer is valid
+        self.assertTrue(serializer.is_valid())
+        # save the account instance
+        account = serializer.save()
+        # assert that the account's user, account_name, and bio attributes are as expected
+        self.assertEqual(account.user, self.user)
+        self.assertEqual(account.account_name, 'default')
+        self.assertEqual(account.bio, 'default statement.')
