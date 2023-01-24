e7f7c2b2e6da0a7f879dcbfc6b088e4df2c570e6
madbunny <madbunny@madbunny.madbunny>
2023-01-24 18:23:11 +0300

    Added category Field to the blog model

--- CMS/settings.py
+++ CMS/settings.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Stori
+from blog.models import Stori,Category
 
 # Register your models here.
 class StoriAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class StoriAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Stori, StoriAdmin)
+admin.site.register(Category)
