61db952af08777666970f6006932ee98b3207fc4
Collins Omariba <collinsomariba@gmail.com>
2023-02-07 12:48:51 +0300

    Add created_at & updated_at in Category model.

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length = 50)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
     class Meta:
