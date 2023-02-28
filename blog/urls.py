ff409bbd6af88f77db1e13835d1c830f17c38bf5
Eugene-Kwaka <kwakaeugene@gmail.com>
2023-02-28 17:14:46 -0600

    Worked on BlogService to enable fetch, create, update and deletion of blogs(stori)

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 
 urlpatterns = [
     path('',views.StoriList.as_view()),
+    path('stori/<int:pk>/',views.StoriDetail.as_view()),
     path('stori/<int:pk>/publish/', views.StoriPublish.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from  rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework import generics
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from blog.serializers import BlogSerializer
 from blog.filters import StoriFilter
@@ -9,13 +10,29 @@ from .throttles import BlogRateThrottle
 from rest_framework.response import Response
 
 
+
 # Create your views here.
-class StoriList(ListAPIView):
-    queryset = Stori.objects.all()   
+# class StoriList(ListAPIView):
+#     queryset = Stori.objects.all()   
+#     serializer_class = BlogSerializer
+#     throttle_classes = [UserRateThrottle, BlogRateThrottle]
+#     filter_backends = (DjangoFilterBackend,)   
+#     filterset_class = StoriFilter
+    
+
+"""List and create mastori using ListCreateAPIView"""
+class StoriList(generics.ListCreateAPIView):
+    queryset = Stori.objects.all()
     serializer_class = BlogSerializer
     throttle_classes = [UserRateThrottle, BlogRateThrottle]
     filter_backends = (DjangoFilterBackend,)   
     filterset_class = StoriFilter
+
+"""Fetch, Update and Delete an individual stori"""
+class StoriDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Stori.objects.all()
+    serializer_class = BlogSerializer
+
 
 class StoriPublish(UpdateAPIView):
     queryset = Stori.objects.all()
@@ -28,3 +45,4 @@ class StoriPublish(UpdateAPIView):
         instance.publish()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+    
