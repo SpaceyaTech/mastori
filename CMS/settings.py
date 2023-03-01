7fd5627aa0f8fd6091511d6e926a2fc74ecec5e2
Jimmy Oty <oduorjames@hotmail.com>
2023-03-01 17:33:25 +0300

    Revert "create postgresql database (#24)" (#32)
    This reverts commit 97e25f0c9d003e26a0f393ce05e862f5c8705cc8.

--- CMS/settings.py
+++ CMS/settings.py
@@ -87,17 +87,14 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-  
+
 DATABASES = {
     'default': {
-       "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DATABASE_DB"),
-        "USER": os.environ.get("DATABASE_USER"),
-        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("DATABASE_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
