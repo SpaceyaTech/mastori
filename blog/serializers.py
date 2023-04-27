67df752197b280694979541b8ae8d7a2a2844c05
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-04-27 17:59:20 +0300

    change read-only field in comment serializer from post to stori

--- blog/serializers.py
+++ blog/serializers.py
@@ -8,7 +8,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
-        read_only_fields = ("account", "post")
+        read_only_fields = ("account", "stori")
 
 
 class CategorySerializers(serializers.ModelSerializer)        :
