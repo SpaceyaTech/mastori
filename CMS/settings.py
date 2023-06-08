b7d26bfb2b8e36ca13fb54fec74ae2cc3734b5aa
JimmyTron <oduorjames@hotmail.com>
2023-06-09 01:04:43 +0300

    Static Files

--- CMS/settings.py
+++ CMS/settings.py
@@ -64,10 +64,13 @@ INSTALLED_APPS = [
 
     # cors
     "corsheaders",
+    # whitenoise
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -153,13 +156,12 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-                          # Default primary key field type
-                          # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-                          )
-
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+                          
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
