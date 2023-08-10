1be89a34862a84311a22fc424284197a95f4a021
Collins Kasyoki <collinskasyoki@gmail.com>
2023-08-10 14:19:17 +0300

    Add tests for category creation

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
-from rest_framework.test import APITestCase
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from accounts.models import User
 from blog.models import Stori, Category
 import json
 
@@ -32,3 +35,35 @@ class TestStoriList(TestCase):
         # Expecting only one object, the one with 'Published' status to be returned
         self.assertEqual(len(mastori_json), 1)
         self.assertTrue(mastori_json[0]['status']=="Published")
+
+class TestCategoryCreation(TestCase):
+    """
+    Test the categories endpoint to ascertain whether
+    only logged in users can create categories
+    """
+    def test_only_logged_in_users_create_category(self):
+        test_user = User.objects.create_user(
+            first_name="Zaphod", 
+            last_name="Beeblebrox", 
+            email="hitchhiker@local.com", 
+            username="hitchhiker", 
+            password="Beeblebrox42")
+        
+        test_data = {
+            'name': 'Test Category 1'
+        }
+
+        categories_url = reverse('categories-list')
+        # This is an UNAUTHORIZED post that bypasses the created user above
+        response = self.client.post(categories_url, test_data)
+        # Check whether the category has NOT been created
+        self.assertEqual(response.status_code, 401)
+
+        c = APIClient()
+        refresh = RefreshToken.for_user(test_user)
+        c.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+        # This is an AUTHORIZED post that gets a token for the created user
+        response = c.post(categories_url, test_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+
