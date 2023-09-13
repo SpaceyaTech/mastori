0ba92b07b95e3fe52b9685882f53f1ad8b55c773
Daniel Ngoya <46535202+BlackHawk0@users.noreply.github.com>
2023-09-13 15:32:05 +0300

    ✨: Added swagger for API documentation (#149)
    * 🔗: swagger documentation urls
    
    * ✨: Adding swagger and drf-spectacular
    
    * 📝: Updated swagger documentation url
    
    * ✨:  Customize schema geneation for API views
    
    * 📦️: Adding swagger and drf spectacular for API documentation

--- CMS/urls.py
+++ CMS/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 admin.site.site_header = "SpaceYaTech CMS Admin"
 admin.site.site_title = "SpaceYaTech Admin Portal"
@@ -17,6 +18,11 @@ urlpatterns = [
     path('',include('blog.urls')),
 
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
+     # Add the Spectacular views for Swagger documentation
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
 ] + static(settings.MEDIA_URL,
            document_root=settings.MEDIA_ROOT)
 
